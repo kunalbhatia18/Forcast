d1d869e1c29e7dac73747a986e1c93b455d42974
kunalbhatia18 <kunal18042002@gmail.com>
2019-03-31 01:16:31 +0530

    Updates

---
+++ precache-manifest.1c7907a4146dc86d769ccaf82696c7b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8c821a1632811be5c72",
+    "url": "/Seasons/static/css/main.e2fa720c.chunk.css"
+  },
+  {
+    "revision": "f8c821a1632811be5c72",
+    "url": "/Seasons/static/js/main.bd321aae.chunk.js"
+  },
+  {
+    "revision": "cd924c742adc5859c0e0",
+    "url": "/Seasons/static/js/runtime~main.fd775556.js"
+  },
+  {
+    "revision": "49e8ed93f0c971c12a69",
+    "url": "/Seasons/static/js/2.11b5b539.chunk.js"
+  },
+  {
+    "revision": "36fc9bf4591e2dda466488535223e789",
+    "url": "/Seasons/index.html"
+  }
+];
