760eeb3b4974cd94c158f097af0c76e80e2949b5
kunalbhatia18 <39365087+kunalbhatia18@users.noreply.github.com>
2019-03-30 22:38:39 +0530

    Update SeasonDisplay.js

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -3,11 +3,11 @@ import './SeasonDisplay.css';
 
 const seasonConfig = {
     summer: {
-        text:"let's hit the beach",
+        text: "let's hit the beach" ,
         iconName: 'sun'
     },
     winter: {
-        text:"burr! it's cold!",
+        text: "burr! it's cold!" ,
         iconName: 'snowflake'
     }
 };
@@ -23,14 +23,13 @@ const getSeason = (lat,month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat,new Date().getMonth())
-    const {text,iconName} = seasonConfig[season];
+    const { text , iconName} = seasonConfig[season];
     return( <div className={`season-display ${season}`}>
     <i className={`icon-left massive ${iconName} icon`} />    
     <h1>
-    {season === 'winter' ? 'Burr! it is chilly': 'Lets hit the beach'}
+    {text}
     </h1>
     <i className={`icon-right massive ${iconName} icon`} />
     </div>);
 };
-
 export default SeasonDisplay;
